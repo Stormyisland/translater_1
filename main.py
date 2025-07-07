6d16bc3f8beccff4f01ec204340f57b441128bcb
Jacques Lavoie <46101717+Stormyisland@users.noreply.github.com>
2025-07-06 23:43:15 -0700

    Update main.py

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ One love no worries.
 The goal deffinently exist.
 Focus commitment and shear fucken will.
 If theres a will theres a way.
-if everything seems under control your not going fast enough.
-to error is human but not AI.
+If everything seems under control your not going fast enough.
+To error is human but not AI.
