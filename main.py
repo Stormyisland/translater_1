59a3845ae3570c57482b7b09863761e17515e0f8
Jacques Lavoie <46101717+Stormyisland@users.noreply.github.com>
2025-07-06 23:42:44 -0700

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,4 @@ The goal deffinently exist.
 Focus commitment and shear fucken will.
 If theres a will theres a way.
 if everything seems under control your not going fast enough.
+to error is human but not AI.
